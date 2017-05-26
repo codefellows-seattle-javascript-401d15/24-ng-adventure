a64d3301d21513c11c3f86002e5bdd0d75014876
Allie Grampa <agrampa@yahoo.com>
2017-05-25 17:57:38 -0700

    added map

--- lab-allie/app/service/map-service.js
+++ lab-allie/app/service/map-service.js
@@ -8,15 +8,45 @@ require('angular')
   let service = {};
   
   service.mapData = {
-    cabin: {
-      description: 'Message from cabin',
-      south: 'trail',
+    room1: {
+      description: 'You are in room 1',
+      down:'room2',
     },
-    trail: {
-      description: 'Message from trail',
-      north: 'cabin',
-      east: 'gate', 
-      south: 'pit',
+    room2: {
+      description: 'You are in room 2',
+      up:'room1',
+      right: 'room3',
+    },
+    room3: {
+      description: 'You are in room 3',
+      left: 'room2',
+      right: 'room5',
+      down: 'room4',
+    },
+    room4: {
+      description: 'You are in room 4',
+      up: 'room3',
+      right: 'room6',
+    },
+    room5: {
+      description: 'You are in room 5',
+      left:'room3',
+      right: 'room8',
+      down: 'room6',
+    },
+    room6: {
+      description: 'You are in room 6',
+      up:'room5',
+      left: 'room4',
+    },
+    room7: {
+      description: 'You are in room 7',
+      down: 'room8',
+    },
+    room8: {
+      description: 'You are in room 8',
+      up:'room7',
+      left: 'room5',
     },
   };
   
