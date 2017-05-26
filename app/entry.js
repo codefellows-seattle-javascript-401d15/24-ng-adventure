9011c18a19cd5ca50d066c298e07b183cbd9ad02
ScottBrenden <scott.b.brenden@gmail.com>
2017-05-25 17:39:39 -0700

    some setup done

---
+++ app/entry.js
@@ -0,0 +1,10 @@
+'use strict'
+
+require('angular')
+  .module('ngAdventure', [])
+
+require('./service/map-service.js')
+require('./service/player-service.js')
+
+require('./component/game-pad/game-pad.js')
+require('./component/player-info/player-info.js')
