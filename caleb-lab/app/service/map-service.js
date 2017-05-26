7ded68e8a3523671818d96f4524cdd915793713d
Caleb <caleb.wells1@gmail.com>
2017-05-26 11:18:51 -0700

    init

---
+++ caleb-lab/app/service/map-service.js
@@ -0,0 +1,72 @@
+'use strict'
+
+require('angular')
+.module('ngAdventure')
+.factory('mapService', ['$log', function($log){
+  $log.debug('Map svc')
+
+  let service = {}
+
+  service.mapData = {
+    Pit: {
+      desc: 'You have clawed your way from the depths of the pit. There is but one way to proceed.',
+      east: 'Courtyard'
+    },
+    Courtyard: {
+      desc: 'You stumble into a ruined courtyard, there is a trail ahead, and a gate to your right.',
+      east: 'Trail',
+      south: 'Graveyard'
+    },
+    Graveyard: {
+      desc: 'The rusty gate screeches in protest as you swing it open. The graveyard is silent. As you pass the worn tombstones, you see only one path ahead - into a dark structure - nothing and noone visible from the outside',
+      north: 'Courtyard',
+      south: 'Crypt'
+    },
+    Crypt: {
+      desc: 'You enter the crypt, and carefully navigate its halls. You enter a chamber with what looks like an altar - smeared with old, dried blood. As you approach the altar and touch it, you hear a click. The altar slides aside to reveal a set of steps hewn into the rock beneath. There is still time to return the way you came',
+      north: 'Graveyard',
+      south: 'Tunnel'
+    },
+    Tunnel: {
+      desc: 'As you clear the bottom step, the altar slides back over the steps, sealing you in. You stumble down the pitch dark tunnel, hands fumbling ahead, until you see a speck of light in the distance. You pick up speed, the back of your neck prickling with the rising terror.',
+      north: 'TreasureRoom2'
+    },
+    TreasureRoom2: {
+      desc: 'You hurl yourself up the steps back into the light, and see a room full of treasure before you',
+      east: 'ThroneRoom'
+    },
+    Trail: {
+      desc: 'You can hear the birds singing as you approach the forest trail. It reminds you of your previous human life, before you became a creature of the pit.',
+      west: 'Courtyard',
+      east: 'Castle'
+    },
+    Castle: {
+      desc: 'You finally see signs of life after walking for hours through the mean forest trail. Smell the stink of humans living in their *cities*. You slink to the Castle in the center of the human cesspool, careful to be unseen.',
+      west: 'Trail',
+    },
+    Hall: {
+      desc: 'You scale the walls of the castle, careful to pull yourself to the top after the sentry passes on his rounds. You sneak into the inner halls of the castle, reaching the Great Hall - nearly to your mark.',
+      north: 'Antechamber',
+      west: 'Castle'
+    },
+    Antechamber: {
+      desc: 'You pass into the Antechamber to the throne room, checking all directions for the knights of the king\'s table before proceeding. You can choose to poison his food in the kitchens to the right, or lie in wait in the throne room itself.',
+      north: 'ThroneRoom',
+      east: 'Kitchens',
+    },
+    Kitchens: {
+      desc: 'You scowl at the stench of humans as you near the kitchens. You can sneak to the king\'s food and poison it, or just steal some of the food.',
+      west: 'Antechamber'
+    },
+    ThroneRoom: {
+      desc: 'You hear sounds from the outside, and jump to the chandelier above. You spit acid onto the stained glass to make a whole big enough to crawl through into the throne room. You see light from a hidden portal behind the throne. You can investigate, or wait to kill the swine of a king, and return to your maker',
+      east: 'TreasureRoom1',
+      south: 'Antechamber'
+    },
+    TreasureRoom1: {
+      desc: 'The King stands across the way, facing away from you. You can end him here, *and* steal treasure to free yourself from the clutches of the dreadlord you serve, or follow the plan, certain that you could neve escape.',
+      east: 'ThroneRoom'
+    }
+  }
+  return service
+}])
---
+++ caleb-lab/app/service/player-service.js
@@ -0,0 +1,60 @@
+'use strict'
+
+require('angular')
+.module('ngAdventure')
+.factory('playerService', ['$q', '$log', 'mapService', function($q, $log, mapService){
+  $log.debug('playerService')
+
+  let service = {}
+  let turn = 0
+  let player = service.player = {
+    name: 'Allie',
+    location: 'Pit',
+    previousLocation: null,
+    directionMoved: null,
+    hp: 10
+  }
+
+  let history = service.history = [
+    {
+      turn,
+      desc: 'You have crawled from the depths of the pit',
+      location: 'Pit',
+      hp: player.hp
+    }
+  ]
+
+  service.movePlayer = function(direction){
+    return new $q((resolve, reject) => {
+      turn ++
+      let currentLocation = player.location
+      let newLocation = mapService.mapData[currentLocation][direction]
+
+      if(!newLocation){
+        history.shift()
+        history.unshift({
+          turn,
+          desc: 'You have run into a dead end',
+          location: currentLocation,
+          previousLocation: history[0].location,
+          directionMoved: direction,
+          hp: --player.hp
+        })
+        return reject('Nothing in that direction, fool.')
+      }
+
+      history.unshift({
+        turn,
+        location: newLocation,
+        previousLocation: currentLocation,
+        desc: mapService.mapData[newLocation].desc,
+        directionMoved: direction,
+        hp: ++player.hp
+      })
+      player.location = newLocation
+      return resolve(player.location)
+    })
+  }
+
+  return service
+}])
