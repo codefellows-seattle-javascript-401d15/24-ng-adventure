e067eca3947c7817c394536af149085ba6edd374
Allie Grampa <agrampa@yahoo.com>
2017-05-29 10:57:33 -0700

    added styling

--- lab-allie/app/entry.js
+++ lab-allie/app/entry.js
@@ -2,11 +2,12 @@
 
 require('angular').module('ngAdventure', []);
 
-// require('./scss/main.scss');
-
 require('./service/map-service');
 require('./service/player-service');
 
 require('./component/game-pad/game-pad.js');
 require('./component/player-info/player-info.js');
 require('./component/history/history.js');
+
+require('./scss/base.scss');
+require('./scss/reset.scss');
