bd0d1241db52aaf8b4f5c83040a1de6c5c579012
Allie Grampa <agrampa@yahoo.com>
2017-05-29 14:14:11 -0700

    added more typeOf and index tests

--- lab-allie/app/test/ngadventure-test.js
+++ lab-allie/app/test/ngadventure-test.js
@@ -19,10 +19,22 @@ describe('Testing the ngAdventure game', function () {
     
     it('should have an initial state', () => {
       expect(this.gamepadCtrl.directions).toEqual(jasmine.any(Array));
+      
       expect(this.gamepadCtrl.directions).toContain('up');
+      expect(this.gamepadCtrl.directions[0]).toEqual('up');
+      expect(this.gamepadCtrl.directions[0]).toEqual(jasmine.any(String));
+      
       expect(this.gamepadCtrl.directions).toContain('down');
+      expect(this.gamepadCtrl.directions[1]).toEqual('down');
+      expect(this.gamepadCtrl.directions[1]).toEqual(jasmine.any(String));
+      
       expect(this.gamepadCtrl.directions).toContain('left');
+      expect(this.gamepadCtrl.directions[2]).toEqual('left');
+      expect(this.gamepadCtrl.directions[2]).toEqual(jasmine.any(String));
+
       expect(this.gamepadCtrl.directions).toContain('right');
+      expect(this.gamepadCtrl.directions[3]).toEqual('right');
+      expect(this.gamepadCtrl.directions[3]).toEqual(jasmine.any(String));
     });
     
     // fit('should move the player', () => {
@@ -46,9 +58,12 @@ describe('Testing the ngAdventure game', function () {
     it('should have an initial state', () => {
       expect(this.historyCtrl.history).toEqual(jasmine.any(Array));
       expect(this.historyCtrl.history[0].turn).toEqual(0);
-      expect(this.historyCtrl.history[0].description).toEqual('Welcome to ngAdventure!');
-      expect(this.historyCtrl.history[0].location).toEqual('Garden');
       expect(this.historyCtrl.history[0].turn).toEqual(jasmine.any(Number));
+      
+      expect(this.historyCtrl.history[0].description).toEqual('Welcome to ngAdventure!');      expect(this.historyCtrl.history[0].description).toEqual(jasmine.any(String));
+      
+      expect(this.historyCtrl.history[0].location).toEqual('Garden');      expect(this.historyCtrl.history[0].location).toEqual(jasmine.any(String));
+
     });
   });
   
@@ -66,9 +81,16 @@ describe('Testing the ngAdventure game', function () {
     
     it('should have an initial state', () => {
       expect(this.playerInfoCtrl.player).toEqual(jasmine.any(Object));
+      
       expect(this.playerInfoCtrl.player.name).toEqual('Player 1');
+      expect(this.playerInfoCtrl.player.name).toEqual(jasmine.any(String));
+      
       expect(this.playerInfoCtrl.player.location).toEqual('garden');
+      expect(this.playerInfoCtrl.player.location).toEqual(jasmine.any(String));
+      
       expect(this.playerInfoCtrl.player.description).toEqual('You are in the Garden');
+      expect(this.playerInfoCtrl.player.description).toEqual(jasmine.any(String));
+      
       expect(this.playerInfoCtrl.player.points).toEqual(100);
       expect(this.playerInfoCtrl.player.points).toEqual(jasmine.any(Number));
     });
