63df4e7094599b43af37db7393facbd341365c63
michaeltreat <michael.leon.treat@gmail.com>
2017-05-31 20:42:40 -0700

    entry.js and html pages laid out, next build the controllers and components

--- lab-miketreat/app/entry.js
+++ lab-miketreat/app/entry.js
@@ -1,1 +1,9 @@
 'use strict';
+
+require('angular').module('ngAdventure', []);
+
+require('./service/map-service.js');
+require('./service/player-info.js');
+
+require('./component/game-pad/game-pad.js');
+require('./component/player-info/player-info.js1');
